4de469d3d6ab779254e49c85e50dfb12f24d4296
Front-line-dev <kimminsung@kakao.com>
2020-08-14 22:45:04 +0900

    feature: animation-bar

---
+++ app.js
@@ -0,0 +1,64 @@
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+
+// the link to your model provided by Teachable Machine export panel
+const URL = "https://teachablemachine.withgoogle.com/models/o9D1N5TN/";
+
+let model, webcam, labelContainer, maxPredictions;
+
+// Load the image model and setup the webcam
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    // Convenience function to setup a webcam
+    const flip = true; // whether to flip the webcam
+    webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+    // append elements to the DOM
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+
+        let cls = document.createElement('div')
+        let span = document.createElement('span')
+        let prog = document.createElement('progress')
+
+        cls.setAttribute('class', 'cls')
+        prog.setAttribute('value','0')
+        prog.setAttribute('max', '100')
+
+        cls.appendChild(span)
+        cls.appendChild(prog)
+        labelContainer.appendChild(cls)
+    }
+    // <div class="cls"><span>123</span><progress value="0" max="100"></progress></div>
+}
+
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPredictions; i++) {
+
+        labelContainer.children[i].firstElementChild.innerHTML = prediction[i].className
+        labelContainer.children[i].lastElementChild.value = (prediction[i].probability*100)
+
+    }
+}
